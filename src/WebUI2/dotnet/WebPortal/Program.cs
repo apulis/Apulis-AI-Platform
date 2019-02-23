138451b148acb12927fea292b34efc3ed153c4b4
Jin Li <jinlmsft@hotmail.com>
2019-02-23 12:20:03 -0800

    Start on .Netcore 2 WebUI

---
+++ src/WebUI2/dotnet/WebPortal/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WindowsAuth
+{
+    public class Program
+    {
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:80")
+                .UseConfiguration(config)
+                .CaptureStartupErrors(true)
+                .UseKestrel(options =>
+                {
+                    /*
+                    options.Listen(IPAddress.Any, 443, listenOptions =>
+                    {
+                        listenOptions.UseHttps("server.pfx");
+                    });*/
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                // context.Database.Migrate();
+                context.Database.EnsureCreated();
+                // requires using Microsoft.Extensions.Configuration;
+                var config = host.Services.GetRequiredService<IConfiguration>();
+                // Set password with the Secret Manager tool.
+                // dotnet user-secrets set SeedUserPW <pw>
+
+                var testUserPw = config["SeedUserPW"];
+                try
+                {
+                    SeedData.Initialize(services, testUserPw).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex.Message, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+    }
+}
---
+++ src/WebUI2/dotnet/WebPortal/models/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsAuth.models
+{
+    public class AppSettings
+    {
+        // RestAPI, workFolderAccess, dataFolderAccess is cluster dependant, as the webUI will support multi-cluster, 
+        // the information will be stored in session. 
+        // public string restapi { get; set; }
+        // public string workFolderAccessPoint { get; set; }
+        // public string dataFolderAccessPoint { get; set; }
+        // public List<string> authorizedGroups { get; set; }
+        // public List<string> adminGroups { get; set; }
+    }
+}

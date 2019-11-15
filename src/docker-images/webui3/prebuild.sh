4dd41d71d4cab4ab0a46ff6609dd662e710112c9
wangsong <wangsong1299@163.com>
2019-11-15 16:26:26 +0800

    feat: dockerfile调整

--- src/docker-images/webui3/prebuild.sh
+++ src/docker-images/webui3/prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 cp -f ../../../../dashboard/package.json .
 cp -f ../../../../dashboard/yarn.lock .
-cp -rf ../../../../dashboard/build .
+# cp -rf ../../../../dashboard/build .
 cp -rf ../../../../dashboard/config .
 cp -rf ../../../../dashboard/public .
 cp -rf ../../../../dashboard/server .

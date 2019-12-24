52445ca296a70a1609e7ccdbed6792e98b6496f4
Jin Li <jinlmsft@hotmail.com>
2019-12-24 14:22:38 -0800

    Default nfs_ssh parameter

--- src/ClusterBootstrap/az_params.py
+++ src/ClusterBootstrap/az_params.py
@@ -18,4 +18,10 @@ default_az_parameters = {
         "nfs_data_disk_path": '/data',
         "nfs_vm": [],
     },
+    "cloud_config" : {
+        "nfs_ssh" : {
+            "port": 22,
+            "source_ips": []
+        },
+    },
 }

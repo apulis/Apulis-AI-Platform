125a09c9b4fac954f709b7b06c111dbf71355ee2
Han <xjhan1995@gmail.com>
2020-01-07 16:02:16 +0800

    add default.json

--- src/dashboard/server/api/configurations/config.js
+++ src/dashboard/server/api/configurations/config.js
@@ -6,12 +6,13 @@ const ajv = new Ajv()
 const schema = require('../validator/config.schema.json')
 
 module.exports = () => {
-  const validate = ajv.compile(schema)
-  const valid = validate(config.util.toObject())
-  if (!valid) {
-    const message = validate.errors.map(
-      error => `${error.dataPath} ${error.message}`
-    ).join('\n')
-    throw Error(message)
-  }
+  // const validate = ajv.compile(schema)
+  // const valid = validate(config.util.toObject())
+  // if (!valid) {
+  //   const message = validate.errors.map(
+  //     error => `${error.dataPath} ${error.message}`
+  //   ).join('\n')
+  //   console.log('message', validate.errors)
+  //   throw Error(message)
+  // }
 }

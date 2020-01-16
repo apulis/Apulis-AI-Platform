c182ca68cc1a4ca1c82dda2a92779cb0403ea9c3
Han <xjhan1995@gmail.com>
2020-01-16 09:21:15 +0800

    remove log

--- src/dashboard/server/api/index.js
+++ src/dashboard/server/api/index.js
@@ -6,22 +6,6 @@ const app = module.exports = new Koa()
 require('./configurations/logger')(app)
 require('./configurations/config')(app)
 
-var fs = require('fs');
-var util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
-var log_stdout = process.stdout;
-
-
-
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
-};
-
-process.on('uncaughtException', (e)=>{  
-  console.log('process error is:', e.message);
-})
-
 const router = require('./router')
 
 app.keys = ['some secret hurr...']

af853059ced59701e73e0807d1b3348a1cdc9226
dragon0486 <869071540@qq.com>
2020-02-10 18:55:12 +0800

    fix bug

--- src/ClusterManager/pod_template.py
+++ src/ClusterManager/pod_template.py
@@ -175,7 +175,7 @@ class PodTemplate():
             # mount /pod
             pod_path = job.get_hostpath(job.job_path, "master")
             pod["mountpoints"].append({"name": "pod", "containerPath": "/pod", "hostPath": pod_path, "enabled": True})
-            pod["init-container"] = os.environ["INIT_CONTAINER_IMAGE"]
+            pod["init-container"] = os.environ.get("INIT_CONTAINER_IMAGE",None)
 
             k8s_pod = self.generate_pod(pod, params["cmd"])
             k8s_pods.append(k8s_pod)

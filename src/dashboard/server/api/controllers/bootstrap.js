43e64a6feb06e2fc1e19c31c16a030709c2f0549
hanxianjie100 <HANXIANJIE100@pingan.com.cn>
2020-02-11 19:57:46 +0800

    删除 bootstrap.js 接口不必要的变量

--- src/dashboard/server/api/controllers/bootstrap.js
+++ src/dashboard/server/api/controllers/bootstrap.js
@@ -3,16 +3,9 @@
  * @property {import('../services/user')} user
  */
 
-const config = require('config')
-
-const frontendConfig = config.has('frontend')
-  ? config.get('frontend')
-  : Object.create(null)
-
 /** @type {import('koa').Middleware<State>} */
 module.exports = (context) => {
   const { user } = context.state
-  const parameter = { config: frontendConfig, user }
   context.type = 'js'
   if (user) {
     context.body = `bootstrap(${JSON.stringify({ ...user, password: '******' })})`

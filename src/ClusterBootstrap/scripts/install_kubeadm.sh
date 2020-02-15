dc7e64a7e80a39d9d15b3403490b04263257e4cd
Jin Li <jinlmsft@hotmail.com>
2020-02-14 17:21:37 -0800

    install kubeadm

---
+++ src/ClusterBootstrap/scripts/install_kubeadm.sh
@@ -0,0 +1,8 @@
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl

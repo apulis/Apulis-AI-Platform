e8e28227aac5eefee696ca17669c12470858d14f
nautilusshell <pengbifeng@qq.com>
2020-02-26 08:53:49 +0000

    增加集群状态检测脚本

--- src/ClusterBootstrap/check/check.py
+++ src/ClusterBootstrap/check/check.py
@@ -56,6 +56,10 @@ import pdb
 from functools import wraps
 
 import types
+from prettytable import PrettyTable
+
+## 全局变量
+check_result = {}   ## 检测结果
 
 
 ## 询问检测是否通过
@@ -65,13 +69,25 @@ def query(func):
     def do_check(*args, **kwargs):
 
         # 执行真正的检测函数
+        print("\n")
+        print("start %s" % (func.__name__))
+
+        print("========================================================================")
+        print("========================================================================")
+
         func(*args, **kwargs)
 
-        ret = input('%s passed? yes or no. \n' % (func.__name__))
+        print("========================================================================")
+        print("========================================================================")
+
+        ret = raw_input('%s passed? type Yes/y or No/n. \n' % (func.__name__))
         if ret.lower() == "y" or ret.lower() == "yes":
             print("passed")
+            check_result[func.__name__] = "yes"
+
         else:
             print("failed")
+            check_result[func.__name__] = "no"
             sys.exit()
 
     return do_check
@@ -85,9 +101,6 @@ class DeploymentChecker(object):
         self.cluster_config = {}
         self.checker_config = {}
 
-        self.cluster_config_file = cluster_config_file
-        self.checker_config_file = checker_config_file
-
         self.cert = ''
         self.cluster_uername = ''
 
@@ -97,6 +110,7 @@ class DeploymentChecker(object):
     ## 设置集群配置
     def set_config(self):
 
+        #pdb.set_trace()
         self.cluster_config = init_config(default_config_parameters)
 
         ## 读取集群配置
@@ -118,29 +132,28 @@ class DeploymentChecker(object):
                 if "clusterId" in tmp:
                     self.cluster_config["clusterId"] = tmp["clusterId"]
         
-        ## ssh拷贝文件到临时目录
+        ## 导入key信息
+        self.load_sshkey()
 
+        return
 
-        ## 更新集群镜像配置、influx db配置、elastic search配置
-        if os.path.exists("../deploy/clusterID.yml"):
-            #update_config()
-            pass
+    def load_sshkey(self):
+
+        if os.path.isfile("../deploy/sshkey/id_rsa"):
+            self.cluster_config["ssh_cert"] = "../deploy/sshkey/id_rsa"
         else:
-            #apply_config_mapping(config, default_config_mapping)
-            #update_docker_image_config()
             pass
 
-        ## 更新集群配置
-        self.process_config()
-        if "ssh_cert" not in config and os.path.isfile("./deploy/sshkey/id_rsa"):
-            config["ssh_cert"] = "./deploy/sshkey/id_rsa"
-        if "ssh_cert" in config:
-            config["ssh_cert"] = expand_path(config["ssh_cert"])
-            
-        config["etcd_user"] = config["admin_username"]
-        config["nfs_user"] = config["admin_username"]
-        config["kubernetes_master_ssh_user"] = config["admin_username"]
+        if "ssh_cert" in self.cluster_config:
+            self.cluster_config["ssh_cert"] = expand_path(self.cluster_config["ssh_cert"])
+        else:
+            pass
 
+        self.cluster_config["etcd_user"] = self.cluster_config["admin_username"]
+        self.cluster_config["nfs_user"] = self.cluster_config["admin_username"]
+        self.cluster_config["kubernetes_master_ssh_user"] = self.cluster_config["admin_username"]
+
+        print(self.cluster_config["ssh_cert"], self.cluster_config["admin_username"])
         return
 
     ## 更新集群配置，譬如增加映射等等
@@ -178,30 +191,133 @@ class DeploymentChecker(object):
         ## 更新docker镜像
         #print "Config:\n{0}\n".format(config)
         if self.cluster_config["kube_custom_scheduler"] or self.cluster_config["kube_custom_cri"]:
+
             if "container" not in self.cluster_config["dockers"]:
                 self.cluster_config["dockers"]["container"] = {}
+
             if "hyperkube" not in self.cluster_config["dockers"]["container"]:
                 self.cluster_config["dockers"]["container"]["hyperkube"] = {}
             # config["dockers"]["container"]["hyperkube"]["fullname"] = config["worker-dockerregistry"] + config["dockerprefix"] + "kubernetes:" + config["dockertag"]
 
         return
 
-    
+    ## 执行所有check_xxx函数
+    def start_check(self):
+        for name in dir(self):
+            if name.startswith('check_'):
+                method = getattr(self, name)
+                method()
+            else:
+                pass
 
-    @query
-    def check_hostname(self):
+        return
+
+    ## 封装ssh函数
+    def cluster_ssh_cmd(self, host, cmd):
+
+        print("【" + host + "】:")
+        utils.SSH_exec_cmd(self.cluster_config["ssh_cert"], 
+            self.cluster_config["admin_username"], 
+            host, 
+            cmd)
+        print("")
+
+        return
+
+    ## 列出所有worker节点
+    def get_worker_nodes(self):
+
+        #pdb.set_trace()
+        return self.get_nodes_from_config("worker")
+
+    ## 列出所有master节点
+    def get_master_nodes(self):
+
+        #pdb.set_trace()
+        return self.get_nodes_from_config("infrastructure")
+
+    ## 获取节点信息
+    def get_nodes_from_config(self, machinerole):
+
+        machinerole = "infrastructure" if machinerole == "infra" else machinerole
+        if "machines" not in self.cluster_config:
+            return []
+
+        else:
+
+            domain = self.get_domain()
+            Nodes = []
+
+            for nodename in self.cluster_config["machines"]:
+
+                nodeInfo = self.cluster_config["machines"][nodename]
+                if "role" in nodeInfo and nodeInfo["role"]==machinerole:
+                    if len(nodename.split("."))<3:
+                        Nodes.append(nodename+domain)
+                    else:
+                        Nodes.append(nodename)
+
+            return sorted(Nodes)
+
+    ## 获取域名
+    def get_domain(self):
+
+        if "network" in self.cluster_config and "domain" in self.cluster_config["network"] and len(self.cluster_config["network"]["domain"]) > 0 :
+            domain = "."+self.cluster_config["network"]["domain"]
+        else:
+            domain = ""
+
+        return domain
+
+    # 打印检测结果
+    def print_summary(self):
+
+        print("========================================================================")
+        print("========================================================================")
+        print("                                                                        ")
+        print("check result: ") 
+
+        global check_result
+        t = PrettyTable(['item', 'status'])
+
+        for key, value in check_result.items():
+            t.add_row([key, value])
+
+        print(t)
         return
 
 
+    ####################################################
+    ## 以下区域开始定义用于执行实际检测步骤的各个函数
+    ## 所有函数必须按照如下格式命名，否则无法执行:
+    ## def check_xxxx
+    ####################################################
 
+    @query
+    def check_sudo(self):
 
+        cmd = 'sudo -v'
+        for host in self.get_master_nodes():
+            self.cluster_ssh_cmd(host, cmd)  
 
-@check
-def check_hostname(host):
-    cmd = "hostnamectl"
-    ssh_exec_cmd("pengbifeng", "pengbifeng", host, cmd)
-    return
+        for host in self.get_worker_nodes():
+            self.cluster_ssh_cmd(host, cmd)    
 
+        return
+
+    @query
+    def check_hostname(self):
+
+        cmd = 'sudo hostnamectl'
+        for host in self.get_master_nodes():
+            self.cluster_ssh_cmd(host, cmd)    
+
+        for host in self.get_worker_nodes():
+            self.cluster_ssh_cmd(host, cmd)    
+
+        return
+
+    
 
 
 def main():
@@ -218,19 +334,18 @@ def main():
         action="store_true"
         )'''
 
-    args = parser.parse_args()
+    #args = parser.parse_args()
     #command = args.command
     #nargs = args.nargs
 
-    config = init_config(default_config_parameters)]
+    config = init_config(default_config_parameters)
     print(config["ssh_cert"], config["admin_username"])
 
-
+    checker = DeploymentChecker()
+    checker.start_check()
+    checker.print_summary()
 
     return
-
-
-
 
 if __name__ == '__main__': 
     main()

01e0760aa0758994c89f5ca9149bcfeb012ecf5a
dragon0486 <869071540@qq.com>
2020-02-27 15:47:17 +0800

    update jwt token for pod template

---
+++ src/utils/jwt_authorization.py
@@ -0,0 +1,25 @@
+import time
+import jwt
+from config import config
+
+def create_jwt_token_for_claims(claims):
+    payload = {
+        'iat': time.time(),
+        'claims': claims,
+        "iss":  config["hostname"],
+        "aud ": config["hostname"],
+        "exp": time.time() + config["jwt"]["token_ttl"],
+        "nbf": time.time()-1
+    }
+    headers = {
+        'alg': config["jwt"]["algorithm"],
+    }
+    return create_jwt_token(payload,config["jwt"]["secret_key"],headers=headers)
+
+def create_jwt_token(payload,secret,algorithm=config["jwt"]["algorithm"],headers=None):
+    jwt_token = jwt.encode(payload,
+                           secret,
+                           algorithm=algorithm,
+                           headers=headers
+                           ).decode('utf-8')
+    return jwt_token

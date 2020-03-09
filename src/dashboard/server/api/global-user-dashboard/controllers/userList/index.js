a56e71c5dc3fd60a1fa74edf5a8caaf8097b54a4
hanxianjie100 <HANXIANJIE100@pingan.com.cn>
2020-03-10 00:12:29 +0800

    查询用户列表时添加角色

--- src/dashboard/server/api/global-user-dashboard/controllers/userList/index.js
+++ src/dashboard/server/api/global-user-dashboard/controllers/userList/index.js
@@ -5,6 +5,17 @@ const clusterIds = Object.keys(config.get('clusters'))
 module.exports = async context => {
   const clusterId = clusterIds[0]
   const response = await new Cluster(context, clusterId).getAllUsers()
+  if (Array.isArray(response)) {
+    response.forEach(r => {
+      r.role = 'Unauthorized'
+      if (r.isAuthorized) {
+        r.role = 'User'
+      }
+      if (r.isAdmin) {
+        r.role = 'Admin'
+      }
+    })
+  }
   context.body = {
     success: true,
     data: response

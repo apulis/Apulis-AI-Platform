e05323dfd3c968b463f1a67ba5db476332b0f07e
hanxianjie100 <HANXIANJIE100@pingan.com.cn>
2020-03-09 23:19:09 +0800

    nodejs获取access时的地址变更

--- src/dashboard/server/api/global-user-dashboard/controllers/userList/index.js
+++ src/dashboard/server/api/global-user-dashboard/controllers/userList/index.js
@@ -4,7 +4,7 @@ const Cluster = require('../../service/cluster')
 const clusterIds = Object.keys(config.get('clusters'))
 module.exports = async context => {
   const clusterId = clusterIds[0]
-  const response = await new Cluster(context, clusterId).fetch('/GetAllUsers')
+  const response = await new Cluster(context, clusterId).GetACL({ userName: context.state.user.userName })
   context.assert(response.ok, 502)
   const data = await response.json()
   context.body = data

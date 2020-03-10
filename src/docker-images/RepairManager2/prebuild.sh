bf72577c725272cb4bdaec0562482ffd9fe2ec5b
dragon0486 <869071540@qq.com>
2020-03-10 19:05:00 +0800

    add repairmanager2

--- src/docker-images/RepairManager2/prebuild.sh
+++ src/docker-images/RepairManager2/prebuild.sh
@@ -2,12 +2,12 @@
 # This command will be executed under directory 
 # src/ClusterBootstrap/deploy/docker-images/.../
 
-rm -rf RepairManager
+rm -rf RepairManager2
 cp -r ../../../../RepairManager RepairManager
 
 # Render config file for email credentials
 cd ../../../
-./deploy.py rendertemplate ./deploy/docker-images/RepairManager/RepairManager/config/email-config.yaml ./deploy/docker-images/RepairManager/RepairManager/config/email-config.yaml
-./deploy.py rendertemplate ./deploy/docker-images/RepairManager/RepairManager/config/rule-config.yaml ./deploy/docker-images/RepairManager/RepairManager/config/rule-config.yaml
-./deploy.py rendertemplate ./deploy/docker-images/RepairManager/RepairManager/config/ecc-config.yaml ./deploy/docker-images/RepairManager/RepairManager/config/ecc-config.yaml
-cd ./deploy/docker-images/RepairManager/
+./deploy.py rendertemplate ./deploy/docker-images/RepairManager2/RepairManager/config/email-config.yaml ./deploy/docker-images/RepairManager2/RepairManager/config/email-config.yaml
+./deploy.py rendertemplate ./deploy/docker-images/RepairManager2/RepairManager/config/rule-config.yaml ./deploy/docker-images/RepairManager2/RepairManager/config/rule-config.yaml
+./deploy.py rendertemplate ./deploy/docker-images/RepairManager2/RepairManager/config/ecc-config.yaml ./deploy/docker-images/RepairManager2/RepairManager/config/ecc-config.yaml
+cd ./deploy/docker-images/RepairManager2/

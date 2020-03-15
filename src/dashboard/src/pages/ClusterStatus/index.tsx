a3077e102e93fc384a8da3eee69af58f468fcac3
hanxianjie100 <HANXIANJIE100@pingan.com.cn>
2020-03-16 01:14:01 +0800

    修复获取 used gpu 为 undefined 的错误

--- src/dashboard/src/pages/ClusterStatus/index.tsx
+++ src/dashboard/src/pages/ClusterStatus/index.tsx
@@ -26,6 +26,7 @@ import {TeamVirtualClusterStatus} from "./components/TeamVirtualClusterStatus";
 import {TeamVCUserStatus} from "./components/TeamVCUserStatus";
 import {ClusterUsage} from "./components/ClusterUsage";
 import {PhysicalClusterNodeStatus} from "./components/PhysicalClusterNodeStatus";
+import { MarkunreadSharp } from "@material-ui/icons";
 
 const ClusterStatus: FC = () => {
   const theme = useTheme();
@@ -165,19 +166,19 @@ const ClusterStatus: FC = () => {
               idleTmp['userName'] = key;
               idleTmp['booked'] = Math.floor(obj['booked'][gpuType] / 3600);
               idleTmp['idle'] = Math.floor(obj['idle'][gpuType] / 3600);
+              idleTmp['idleGPU'] = 0;
               fetchIdes.push(idleTmp);
             }
           }
         }
-        const fetchUserGPUUrl = (userfetchs['getIdleGPUPerUserUrl']+params + '&time=' + ((new Date().getTime() / 1000)));
+        const fetchUserGPUUrl = (userfetchs['getIdleGPUPerUserUrl']+params + '&time=' + Math.floor((1584265252)));
         fetch(fetchUserGPUUrl).then(async (response: any) => {
           const res = await response.json();
-          console.log('res.data', res)
           for (let item of res["data"]["result"]) {
             let idleUser: any = {};
             idleUser['userName'] = item['metric']['username'];
-            idleUser['gpuType'] = item['metric']['gpu_Type']
-            idleUser['idleGPU'] = item['value'][1];
+            idleUser['gpuType'] = item['metric']['gpu_type'];
+            idleUser['idleGPU'] = Number(item['value'][1]);
             prometheusResp.push(idleUser)
           }
           // fetchIdes
@@ -200,7 +201,15 @@ const ClusterStatus: FC = () => {
               mu['preemptableGPU'] = 0;
             }
           });
-          let finalUserStatus = _.values(mergePropertyByKey(tmpMerged, prometheusResp, 'userName'));
+          prometheusResp.forEach((val1: any) => {
+            const { userName, gpuType } = val1;
+            tmpMerged.forEach((val2: any) => {
+              if (userName === val2.userName && gpuType === val2.gpuType) {
+                val2.idleGPU = val1.idleGPU;
+              }
+            })
+          })
+          let finalUserStatus = tmpMerged;
           let totalRow: any = {};
           totalRow['booked'] = 0;
           totalRow['idle'] = 0;

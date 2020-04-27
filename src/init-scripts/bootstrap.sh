367336fe60434a74820c47542a1b260e2e185b13
nautilusshell <bifeng.peng@apulis.com>
2020-04-27 14:09:29 +0800

    bugfix:   fixed the issue that calling apt-update from bootstrap.sh failed with connection fails error message

--- src/init-scripts/bootstrap.sh
+++ src/init-scripts/bootstrap.sh
@@ -5,8 +5,12 @@ set -ex
 sh -x /dlts-runtime/install.sh
 
 # set apt mirrors for foreign sources
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sed -i 's|https\?://[^/]\+/|http://mirrors.aliyun.com/|' /etc/apt/sources.list && apt-get clean && apt-get update
+#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sed -i 's|https\?://[^/]\+/|http://mirrors.aliyun.com/|' /etc/apt/sources.list 
+
+# to avoid apt-get update error: 
+# download.nvidia.cn: connection timeout
+rm -rf /etc/apt/sources.list.d/cuda.list /etc/apt/sources.list.d/cuda.list.save /etc/apt/sources.list.d/graphics-drivers-ubuntu-ppa-bionic.list /etc/apt/sources.list.d/nvidia-ml.list /etc/apt/sources.list.d/nvidia-ml.list.save
 
 SCRIPT_DIR=/pod/scripts
 echo bootstrap starts at `date` &>> ${LOG_DIR}/bootstrap.log

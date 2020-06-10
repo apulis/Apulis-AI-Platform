09885188f0a6f5099a7f8a18886af5bc16031405
Nautilusshell <39021950+nautilusshell@users.noreply.github.com>
2020-06-10 17:37:53 +0800

    update

--- src/init-scripts/setup_ssh_config.sh
+++ src/init-scripts/setup_ssh_config.sh
@@ -26,8 +26,12 @@ host_list="${ps_host_list} ${worker_host_list}"
 # generate ~/.ssh/config
 SSH_CONFIG_FILE=/home/${DLWS_USER_NAME}/.ssh/config
 IB_CONFIG_FILE=/home/${DLWS_USER_NAME}/.ssh/ib_config
+NPU_CONFIG_FILE=/home/${DLWS_USER_NAME}/.ssh/npu_config
+
 >${SSH_CONFIG_FILE}
 >${IB_CONFIG_FILE}
+>${NPU_CONFIG_FILE}
+
 chown ${DLWS_USER_NAME} ${SSH_CONFIG_FILE}
 chmod 600 ${SSH_CONFIG_FILE}
 
@@ -37,6 +41,7 @@ do
     then
         ip=$DLWS_SD_SELF_IP
         port=$DLWS_SD_SELF_SSH_PORT
+        host_ip=$DLWS_SD_SELF_HOST_IP
     else
         role=${host%%-*}
         idx=${host##*-}
@@ -44,9 +49,16 @@ do
         ip_key=DLWS_SD_${role}${idx}_IP
         ib_ip_key=DLWS_SD_${role}${idx}_IB_IP
         port_key=DLWS_SD_${role}${idx}_SSH_PORT
+
+        npu_ip_list_key=DLWS_SD_${role}${idx}_SSH_PORT
+        host_ip_key=DLWS_SD_${idx}_HOST_IP
+
         ip=$(printenv $ip_key)
         ib_ip=$(printenv $ib_ip_key)
         port=$(printenv $port_key)
+
+        npu_ip_list=$(printenv $npu_ip_list_key)
+        host_ip=$(printenv $host_ip_key)
     fi
     cat >>${SSH_CONFIG_FILE} <<EOF
 
@@ -58,14 +70,24 @@ Host ${host}
   UserKnownHostsFile /dev/null
 
 EOF
+
+# generate ib related info
 if [ ! -z ib_ip ] && [ "$role" = "worker"]; then
     cat >> ${IB_CONFIG_FILE} << EOF
 $(ib_ip) slots=${DLWS_NUM_GPU_PER_WORKER}
 EOF
+
 fi
     # also add entry to /etc/hosts
     echo -e "${ip}\t${host}" >> /etc/hosts
 done
+
+# generate npu related info
+if [ ! -z npu_ip_list ] && [ "$role" = "worker"]; then
+    cat >> ${NPU_CONFIG_FILE} << EOF
+$(npu_ip_list) slots=${DLWS_NUM_GPU_PER_WORKER}
+$(host_ip) slots=${DLWS_NUM_GPU_PER_WORKER}
+EOF
 
 envs=(
 LD_LIBRARY_PATH

40abc90f75ea2e6087c51fc5b0eea8f21bbbfdbc
nautilusshell <bifeng.peng@apulis.com>
2020-06-28 06:41:48 +0000

    update kubeadm script

--- src/ClusterBootstrap/scripts/install_kubeadm.sh
+++ src/ClusterBootstrap/scripts/install_kubeadm.sh
@@ -9,4 +9,4 @@ k8s_version="1.18.0-00"
 
 sudo apt-get update
 sudo apt-get install -y kubelet=${k8s_version} kubeadm=${k8s_version} kubectl=${k8s_version}
-#sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl

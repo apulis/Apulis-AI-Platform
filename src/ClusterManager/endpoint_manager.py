ec7a45497b7d4751e230a1d7d53d7b093ab1c2ff
dragon0486 <869071540@qq.com>
2020-06-29 15:44:24 +0800

    fixbug

--- src/ClusterManager/endpoint_manager.py
+++ src/ClusterManager/endpoint_manager.py
@@ -99,7 +99,7 @@ def setup_ssh_server(user_name, pod_name, host_network=False):
 
 
 def setup_jupyter_server(user_name, pod_name,jupyter_port,nodePort):
-    bash_script = "sudo bash -c 'export DEBIAN_FRONTEND=noninteractive; if ! [ -x \"$(command -v jupyter)\" ];then apt-get update &&  umask 022 && apt-get install -y python3-pip && python3 -m pip install --upgrade pip && python3 -m pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/ && python3 -m pip install jupyterlab;fi && touch /job/jupyter.log && chmod 777 /job/jupyter.log && cd /home/" + user_name + " && chmod -R 777 ./.local &&  runuser -l " + user_name + " -c \"jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token= --port=" + str(jupyter_port) + " --NotebookApp.base_url=/endpoints/"+str(nodePort)+ "/ --NotebookApp.allow_origin='*' &>/job/jupyter.log &\"'"
+    bash_script = "sudo bash -c 'export DEBIAN_FRONTEND=noninteractive; if ! [ -x \"$(command -v jupyter)\" ];then apt-get update &&  umask 022 && apt-get install -y python3-pip && python3 -m pip install --upgrade pip && python3 -m pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/ && python3 -m pip install jupyterlab;fi && touch /job/jupyter.log && chmod 777 /job/jupyter.log && cd /home/" + user_name + " && chmod -R 777 ./.local && chmod -R 777 ./.jupyter &&  runuser -l " + user_name + " -c \"jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token= --port=" + str(jupyter_port) + " --NotebookApp.base_url=/endpoints/"+str(nodePort)+ "/ --NotebookApp.allow_origin='*' &>/job/jupyter.log &\"'"
     output = k8sUtils.kubectl_exec("exec %s %s" % (pod_name, " -- " + bash_script))
     if output != "":
         raise Exception("Failed to start jupyter server in container. JobId: %s " % pod_name)

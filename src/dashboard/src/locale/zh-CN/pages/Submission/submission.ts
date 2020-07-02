179ee06631a404d340389585b4b90192a5eda0fc
JiangWei <Wei.Jiang@apulis.com>
2020-07-02 10:48:59 +0800

    0702_V2_汉化bug处理_wei_jiang

--- src/dashboard/src/locale/zh-CN/pages/Submission/submission.ts
+++ src/dashboard/src/locale/zh-CN/pages/Submission/submission.ts
@@ -38,7 +38,7 @@ const advance = {
     environmentVariableValue:'环境变量值',
     jobNameIsRequired:'请输入任务名',
     dockerImageIsRequired:'请输入docker镜像',
-    commandIsRequired:'请输入命令',
+    commandIsRequired:'请输入指令',
     tensorboardListenTips:'在docker容器中，tensorboard将监听的目录：~/tensorboard/<JobId>/logs'
 }
 const template = {

211d66f3976984c42d58bb95354e2a5f4a174c40
Ti-tanium <1158976264@qq.com>
2020-07-13 10:43:50 +0800

    解决汉化小bug

--- src/dashboard/src/pages/Model/index.tsx
+++ src/dashboard/src/pages/Model/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, FC, useEffect, useMemo, useContext, useCallback } from "react";
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, RadioGroup, Radio, CircularProgress,
-  FormControlLabel, TextField, MenuItem } from "@material-ui/core";
+import {
+  Button, Dialog, DialogTitle, DialogContent, DialogActions, RadioGroup, Radio, CircularProgress,
+  FormControlLabel, TextField, MenuItem
+} from "@material-ui/core";
 import MaterialTable, { Column, Options, Action } from 'material-table';
 import { renderDate, sortDate } from '../JobsV2/tableUtils';
 import './index.less';
@@ -17,12 +19,12 @@ import { useTranslation } from "react-i18next";
 
 const Model: React.FC = () => {
   const { t } = useTranslation();
-  const inferenceNameTip=t('Inference Name is required！')
-  const inputPathTip=t('Input Path is required！')
-  const outputPathTip=t('Output Path is required！')
-  const URLTip=t('URL is required！')
-  const usernameTip=t('Username is required！')
-  const passwordTip=t('Password is required！')
+  const inferenceNameTip = t('Inference Name is required！');
+  const inputPathTip = t('Input Path is required！');
+  const outputPathTip = t('Output Path is required！');
+  const URLTip = t('URL is required！');
+  const usernameTip = t('Username is required！');
+  const passwordTip = t('Password is required！');
   const { currentRole = [], userName } = useContext(AuthContext);
   const getDate = (item: any) => new Date(item.jobTime);
   const isAdmin = currentRole.includes('System Admin');
@@ -49,14 +51,6 @@ const Model: React.FC = () => {
     if (modelconversionStatus === 'converting') status = jobStatus === 'finished' ? t('convert success') : jobStatus === 'failed' ? 'convert failed' : modelconversionStatus;
     return <p>{status}</p>
   }
-  const columns = useMemo<Array<Column<any>>>(() => [
-    { title: 'ID', type: 'string', field: 'jobId', sorting: false, cellStyle: {fontFamily: 'Lucida Console'}},
-    { title: t('Inference Name'), type: 'string', field: 'jobName', sorting: false},
-    { title: t('Type'), type: 'string', field: 'modelconversionType', sorting: false },
-    { title: t('Time'), type: 'datetime', field: 'jobTime', 
-      render: renderDate(getDate), customSort: sortDate(getDate) },
-    { title: t('Status'), type: 'string', field: 'modelconversionStatus', render: renderStatus },
-  ], []);
   const options = useMemo<Options>(() => ({
     padding: 'dense',
     actionsColumnIndex: -1,
@@ -83,10 +77,10 @@ const Model: React.FC = () => {
   const getFdInfo = () => {
     let info = {};
     axios.get(`/${selectedCluster}/getFDInfo`)
-    .then((res: { data: any; }) => {
-      setFdInfo(res.data);
-      info = res.data;
-    })
+      .then((res: { data: any; }) => {
+        setFdInfo(res.data);
+        info = res.data;
+      })
     return info;
   }
 
@@ -98,11 +92,11 @@ const Model: React.FC = () => {
   useInterval(() => {
     getData();
   }, pollInterval);
-  
+
   const onPush = async (e: any, item: any) => {
     const info = await getFdInfo();
-    const successTip=t('Push Inference successfully！')
-    const errorTip=t('Please enter settings first')
+    const successTip = t('Push Inference successfully！');
+    const errorTip = t('Please enter settings first');
     if (info) {
       const { jobId } = item;
       setPushId(jobId);
@@ -121,7 +115,7 @@ const Model: React.FC = () => {
 
   const push = useCallback((job: any): Action<any> => {
     const { jobStatus, modelconversionStatus, jobId } = job;
-    const pushInference=t('Push Inference')
+    const pushInference = t('Push Inference');
     const disabled = (!(modelconversionStatus === 'converting' && jobStatus === 'finished') || pushId === jobId);
     return {
       icon: 'backup',
@@ -137,8 +131,8 @@ const Model: React.FC = () => {
 
   const onSubmitTransform = async (val: any) => {
     setBtnLoading(true);
-    const successTip=t('Edge Inference successfully！')
-    const errorTip=t('Edge Inference failed！')
+    const successTip = t('Edge Inference successfully！');
+    const errorTip = t('Edge Inference failed！');
     await axios.post(`/${selectedCluster}/teams/${selectedTeam}/postModelConversionJob`, {
       ...val,
       image: '',
@@ -148,7 +142,7 @@ const Model: React.FC = () => {
       getData();
       message('success', successTip);
       setModalFlag1(false);
-    },  () => {
+    }, () => {
       message('error', errorTip);
     })
     setBtnLoading(false);
@@ -156,12 +150,12 @@ const Model: React.FC = () => {
 
   const onSubmitSettings = async (val: any) => {
     setBtnLoading(true);
-    const successTip=t('Save Settings successfully！')
-    const errorTip=t('Save Settings failed！')
+    const successTip = t('Save Settings successfully！');
+    const errorTip = t('Save Settings failed！');
     await axios.post(`/${selectedCluster}/teams/${selectedTeam}/setFDInfo`, val).then((res: any) => {
       message('success', successTip);
       setModalFlag2(false);
-    },  () => {
+    }, () => {
       message('error', errorTip);
     })
     setBtnLoading(false);
@@ -204,144 +198,178 @@ const Model: React.FC = () => {
       </div>
       <MaterialTable
         title=""
-        columns={columns}
+        columns={[
+          { title: 'ID', type: 'string', field: 'jobId', sorting: false, cellStyle: { fontFamily: 'Lucida Console' } },
+          { title: t('Inference Name'), type: 'string', field: 'jobName', sorting: false },
+          { title: t('Type'), type: 'string', field: 'modelconversionType', sorting: false },
+          {
+            title: t('Time'), type: 'datetime', field: 'jobTime',
+            render: renderDate(getDate), customSort: sortDate(getDate)
+          },
+          { title: t('Status'), type: 'string', field: 'modelconversionStatus', render: renderStatus },
+        ]}
         data={jobs}
         options={options}
         actions={[push]}
         onChangeRowsPerPage={(pageSize: any) => setPageSize(pageSize)}
+        localization={{
+          pagination: {
+            labelDisplayedRows: '{from}-{to} of {count}',
+            firstTooltip: t('jobsV2.firstPage'),
+            previousTooltip: t('jobsV2.previousPage'),
+            nextTooltip: t('jobsV2.nextPage'),
+            lastTooltip: t('jobsV2.lastPage'),
+            labelRowsPerPage: t('jobsV2.labelRowsPerPage'),
+            labelRowsSelect: t('jobsV2.labelRowsSelect')
+          },
+          toolbar: {
+            nRowsSelected: '{0} row(s) selected',
+            searchTooltip: t('jobsV2.search'),
+            searchPlaceholder: t('jobsV2.search')
+          },
+          header: {
+            actions: t('jobsV2.actions')
+          },
+          body: {
+            emptyDataSourceMessage: t('jobsV2.noRecordsToDisplay'),
+            filterRow: {
+              filterTooltip: 'Filter'
+            }
+          }
+        }}
       />
-      {modalFlag1 && 
-      <Dialog open={modalFlag1} disableBackdropClick fullWidth>
-        <DialogTitle>{t("New Inference")}</DialogTitle>
-        <form onSubmit={handleSubmit(onSubmitTransform)}>
-          <DialogContent dividers>
-            {/* <RadioGroup row aria-label="quiz" name="quiz" value={type} onChange={onTypeChange}>
+      {modalFlag1 &&
+        <Dialog open={modalFlag1} disableBackdropClick fullWidth>
+          <DialogTitle>{t("New Inference")}</DialogTitle>
+          <form onSubmit={handleSubmit(onSubmitTransform)}>
+            <DialogContent dividers>
+              {/* <RadioGroup row aria-label="quiz" name="quiz" value={type} onChange={onTypeChange}>
               <FormControlLabel value="1" control={<Radio />} label="Caffe -> A310" />
               <FormControlLabel value="2" control={<Radio />} label="TensorFlow -> A310" />
             </RadioGroup> */}
-            {/* <SelectTree label="Input Path" style={{ margin: '10px 0' }} formObj={formObj} name="in" />
+              {/* <SelectTree label="Input Path" style={{ margin: '10px 0' }} formObj={formObj} name="in" />
             <SelectTree label="Output Path" style={{ margin: '10px 0' }} formObj={formObj} name="out" /> */}
-            <TextField
-              label={t("Inference Name")}
-              name="jobName"
-              fullWidth
-              variant="filled"
-              error={Boolean(errors.jobName)}
-              helperText={errors.jobName ? errors.jobName.message : ''}
-              InputLabelProps={{ shrink: true }}
-              inputProps={{ maxLength: 20 }}
-              style={{ margin: '10px 0' }}
-              inputRef={register({
-                required: inferenceNameTip,
-                pattern: {
-                  value: NameReg,
-                  message: NameErrorText
-                }
-              })}
-            />
-            <TextField
-              select
-              label={t("Type")}
-              name="conversionType"
-              fullWidth
-              onChange={e => setType(e.target.value)}
-              variant="filled"
-              value={type}
-              style={{ margin: '10px 0' }}
-            >
-              {getConvertionTypeOptions()}
-            </TextField>
-            <TextField
-              label={t("Input Path")}
-              name="inputPath"
-              fullWidth
-              variant="filled"
-              error={Boolean(errors. inputPath)}
-              helperText={errors. inputPath ? errors. inputPath.message : ''}
-              InputLabelProps={{ shrink: true }}
-              style={{ margin: '10px 0' }}
-              inputRef={register({ 
-                required: inputPathTip,
-              })}
-            />
-            <TextField
-              label={t("Output Path")}
-              name="outputPath"
-              fullWidth
-              variant="filled"
-              error={Boolean(errors.outputPath)}
-              helperText={errors.outputPath ? errors.outputPath.message : ''}
-              InputLabelProps={{ shrink: true }}
-              style={{ margin: '10px 0' }}
-              inputRef={register({
-                required: outputPathTip,
-              })}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => onCloseDialog(1)} color="primary" variant="outlined">{t('Cancel')}</Button>
-            <Button type="submit" color="primary" variant="contained" disabled={btnLoading} style={{ marginLeft: 8 }}>
-              {btnLoading && <CircularProgress size={20}/>}{t('Submit')}
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>}
-      {modalFlag2 && 
-      <Dialog open={modalFlag2} disableBackdropClick fullWidth>
-        <DialogTitle>{t("Settings")}</DialogTitle>
-        <form onSubmit={handleSubmit(onSubmitSettings)}>
-          <DialogContent dividers>
-            <TextField
-              label="URL"
-              name="url"
-              fullWidth
-              variant="filled"
-              error={Boolean(errors.url)}
-              defaultValue={fdInfo.url}
-              helperText={errors.url ? errors.url.message : ''}
-              InputLabelProps={{ shrink: true }}
-              inputRef={register({
-                required: URLTip
-              })}
-            />
-            <TextField
-              label={t("Username")}
-              name="username"
-              fullWidth
-              variant="filled"
-              defaultValue={fdInfo.username}
-              error={Boolean(errors.username)}
-              helperText={errors.username ? errors.username.message : ''}
-              InputLabelProps={{ shrink: true }}
-              inputRef={register({
-                required: usernameTip
-              })}
-              style={{ margin: '20px 0' }}
-            />
-            <TextField
-              label={t("Password")}
-              name="password"
-              fullWidth
-              variant="filled"
-              defaultValue={fdInfo.password}
-              error={Boolean(errors.password)}
-              helperText={errors.password ? errors.password.message : ''}
-              InputLabelProps={{ shrink: true }}
-              inputRef={register({
-                required: passwordTip
-              })}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => onCloseDialog(2)} color="primary" variant="outlined">{t('Cancel')}</Button>
-            <Button type="submit" color="primary" disabled={btnLoading} variant="contained" style={{ marginLeft: 8 }}>
-              {btnLoading && <CircularProgress size={20}/>}{("Save")}
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>}
+              <TextField
+                label={t("Inference Name")}
+                name="jobName"
+                fullWidth
+                variant="filled"
+                error={Boolean(errors.jobName)}
+                helperText={errors.jobName ? errors.jobName.message : ''}
+                InputLabelProps={{ shrink: true }}
+                inputProps={{ maxLength: 20 }}
+                style={{ margin: '10px 0' }}
+                inputRef={register({
+                  required: inferenceNameTip,
+                  pattern: {
+                    value: NameReg,
+                    message: NameErrorText
+                  }
+                })}
+              />
+              <TextField
+                select
+                label={t("Type")}
+                name="conversionType"
+                fullWidth
+                onChange={e => setType(e.target.value)}
+                variant="filled"
+                value={type}
+                style={{ margin: '10px 0' }}
+              >
+                {getConvertionTypeOptions()}
+              </TextField>
+              <TextField
+                label={t("Input Path")}
+                name="inputPath"
+                fullWidth
+                variant="filled"
+                error={Boolean(errors.inputPath)}
+                helperText={errors.inputPath ? errors.inputPath.message : ''}
+                InputLabelProps={{ shrink: true }}
+                style={{ margin: '10px 0' }}
+                inputRef={register({
+                  required: inputPathTip,
+                })}
+              />
+              <TextField
+                label={t("Output Path")}
+                name="outputPath"
+                fullWidth
+                variant="filled"
+                error={Boolean(errors.outputPath)}
+                helperText={errors.outputPath ? errors.outputPath.message : ''}
+                InputLabelProps={{ shrink: true }}
+                style={{ margin: '10px 0' }}
+                inputRef={register({
+                  required: outputPathTip,
+                })}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => onCloseDialog(1)} color="primary" variant="outlined">{t('Cancel')}</Button>
+              <Button type="submit" color="primary" variant="contained" disabled={btnLoading} style={{ marginLeft: 8 }}>
+                {btnLoading && <CircularProgress size={20} />}{t('Submit')}
+              </Button>
+            </DialogActions>
+          </form>
+        </Dialog>}
+      {modalFlag2 &&
+        <Dialog open={modalFlag2} disableBackdropClick fullWidth>
+          <DialogTitle>{t("Settings")}</DialogTitle>
+          <form onSubmit={handleSubmit(onSubmitSettings)}>
+            <DialogContent dividers>
+              <TextField
+                label="URL"
+                name="url"
+                fullWidth
+                variant="filled"
+                error={Boolean(errors.url)}
+                defaultValue={fdInfo.url}
+                helperText={errors.url ? errors.url.message : ''}
+                InputLabelProps={{ shrink: true }}
+                inputRef={register({
+                  required: URLTip
+                })}
+              />
+              <TextField
+                label={t("Username")}
+                name="username"
+                fullWidth
+                variant="filled"
+                defaultValue={fdInfo.username}
+                error={Boolean(errors.username)}
+                helperText={errors.username ? errors.username.message : ''}
+                InputLabelProps={{ shrink: true }}
+                inputRef={register({
+                  required: usernameTip
+                })}
+                style={{ margin: '20px 0' }}
+              />
+              <TextField
+                label={t("Password")}
+                name="password"
+                fullWidth
+                variant="filled"
+                defaultValue={fdInfo.password}
+                error={Boolean(errors.password)}
+                helperText={errors.password ? errors.password.message : ''}
+                InputLabelProps={{ shrink: true }}
+                inputRef={register({
+                  required: passwordTip
+                })}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => onCloseDialog(2)} color="primary" variant="outlined">{t('Cancel')}</Button>
+              <Button type="submit" color="primary" disabled={btnLoading} variant="contained" style={{ marginLeft: 8 }}>
+                {btnLoading && <CircularProgress size={20} />}{("Save")}
+              </Button>
+            </DialogActions>
+          </form>
+        </Dialog>}
     </div>
-  ); 
+  );
 };
 
 export default Model;

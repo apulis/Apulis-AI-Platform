37f7d8bd907284721ba91c1fce0f0b41eff667d1
dragon0486 <869071540@qq.com>
2020-07-14 23:49:27 +0800

    update inference

---
+++ src/utils/inference.py
@@ -0,0 +1,36 @@
+import requests
+import base64
+import numpy as np
+import cv2
+import serve_utils as utils
+from PIL import Image
+
+def object_classifier_infer(inference_url,image,signature_name):
+    res = requests.post(inference_url, json={
+        "signature_name": signature_name,
+        "instances": [
+            {"image_bytes": {"b64": base64.b64encode(image).decode("utf-8")}
+             }
+        ]})
+    ret = res.json().get("predictions")
+    return ret
+
+def object_detaction_infer(inference_url,imageFile,signature_name):
+    img_size = 608
+    original_image = cv2.imdecode(np.frombuffer(imageFile, np.uint8), -1)
+    image_data = utils.image_preporcess(np.copy(original_image), [img_size, img_size])
+    image_data_yolo_list = image_data[np.newaxis, :].tolist()
+    headers = {"Content-type": "application/json"}
+    r = requests.post(inference_url,headers=headers,json={
+        "signature_name": signature_name,
+        "instances":image_data_yolo_list
+    }).json()
+    output = np.array(r['predictions'])
+    output = np.reshape(output, (-1, 85))
+    original_image_size = original_image.shape[:2]
+    bboxes = utils.postprocess_boxes(output, original_image_size, img_size, 0.3)
+    bboxes = utils.nms(bboxes, 0.45, method='nms')
+    image = utils.draw_bbox(original_image, bboxes)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = Image.fromarray(image)
+    return image

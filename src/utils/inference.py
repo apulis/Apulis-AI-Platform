af219ca6c6ab88795ad95b503126ff481cd053b2
dragon0486 <869071540@qq.com>
2020-08-06 16:31:22 +0800

    fixbug

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -1,11 +1,11 @@
 import requests
 import base64
 import numpy as np
-import cv2
-import serve_utils
 from PIL import Image
 import os,io
 import json
+from io import BytesIO
+import visualization_utils
 
 def object_classifier_infer(inference_url,image,signature_name):
     res = requests.post(inference_url, json={
@@ -18,11 +18,14 @@ def object_classifier_infer(inference_url,image,signature_name):
     return ret
 
 def object_detaction_infer(inference_url,imageFile,signature_name):
+    import serve_utils
+    import cv2
     img_size = 608
     original_image = cv2.imdecode(np.frombuffer(imageFile, np.uint8), -1)
     image_data = serve_utils.image_preporcess(np.copy(original_image), [img_size, img_size])
     image_data_yolo_list = image_data[np.newaxis, :].tolist()
     headers = {"Content-type": "application/json"}
+    inference_url = "http://127.0.0.1/endpoints/"+inference_url.split("/endpoints/")[1]
     r = requests.post(inference_url,headers=headers,
                       data=json.dumps({"signature_name": signature_name,"instances":image_data_yolo_list}))
     r = r.json()
@@ -34,6 +37,48 @@ def object_detaction_infer(inference_url,imageFile,signature_name):
     image = serve_utils.draw_bbox(original_image, bboxes)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image = Image.fromarray(image)
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr,format='JPEG')
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+def read_class_names2(class_file_name):
+    '''loads class name from a file'''
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data,1):
+            print(name)
+            names[ID] = {"name":name.strip('\n'),"id":ID}
+    return names
+
+def object_detaction_infer2(inference_url,imageFile,signature_name):
+    image = Image.open(BytesIO(imageFile))
+    (im_width, im_height) = image.size
+    image_data = np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+    image_data_yolo_list = image_data[np.newaxis, :].tolist()
+    headers = {"Content-type": "application/json"}
+    inference_url = "http://127.0.0.1/endpoints/"+inference_url.split("/endpoints/")[1]
+    r = requests.post(inference_url,headers=headers,
+                      data=json.dumps({"signature_name": signature_name,"instances":image_data_yolo_list}))
+    r = r.json()
+    output_dict = r['predictions'][0]
+    output_dict['num_detections'] = int(output_dict['num_detections'])
+    output_dict['detection_classes'] = np.array([int(class_id) for class_id in output_dict['detection_classes']])
+    output_dict['detection_boxes'] = np.array(output_dict['detection_boxes'])
+    output_dict['detection_scores'] = np.array(output_dict['detection_scores'])
+    category_index = read_class_names2("coco.names")
+    visualization_utils.visualize_boxes_and_labels_on_image_array(
+        image_data,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=1,
+    )
+    Image.fromarray(image_data).show()
+    image = Image.fromarray(image_data)
     imgByteArr = io.BytesIO()
     image.save(imgByteArr,format='JPEG')
     imgByteArr = imgByteArr.getvalue()

1a30628409a9b6e5c4c4ef276729cf7dc3c5fbd2
dragon0486 <869071540@qq.com>
2020-08-11 10:18:58 +0800

    fixbug

--- src/ClusterManager/dataset_convert.py
+++ src/ClusterManager/dataset_convert.py
@@ -99,6 +99,8 @@ def merge_json_to_coco_dataset(list_ppath,json_path,coco_file_path,prefix="",arg
                     i["area"] = int(mask.area(i["segmentation"]))
                 if i["bbox"]:
                     i["area"] = i["bbox"][2] * i["bbox"][3]
+            if "iscrowd" not in i:
+                i["iscrowd"] = 0
         coco["images"].extend(json_dict["images"])
         coco["annotations"].extend(json_dict["annotations"])
         # source_path = os.path.join(json_path, 'images', "{}.jpg".format(ImgID))

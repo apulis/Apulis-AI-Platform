c58425623d1e74c82e0716b72b187ae6a05a1a21
dragon0486 <869071540@qq.com>
2020-08-21 14:38:53 +0800

    fixbug

--- src/ClusterManager/job_manager.py
+++ src/ClusterManager/job_manager.py
@@ -455,6 +455,10 @@ def check_job_status(job_id):
             logger.info("Job: {}, Succeeded!".format(job_id))
             return "Succeeded", []
 
+    # for distribute job(only npu),if all worker is finished,master(for npu,sleep infinity) is also finished.
+    if all([job_role.status()=="Succeeded" for job_role in job_roles if job_role.role_name=="worker"]):
+        return "Succeeded", []
+
     statuses = [job_role.status() for job_role in job_roles]
     logger.info("Job: {}, status: {}".format(job_id, statuses))
 

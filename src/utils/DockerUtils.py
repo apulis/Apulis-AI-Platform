4363657a318503779348d37f4c699be494c8a9e2
250lth <gang250lth@gmail.com>
2020-08-31 15:22:25 +0800

    fix version error

--- src/utils/DockerUtils.py
+++ src/utils/DockerUtils.py
@@ -355,7 +355,8 @@ def modify_docker_tag_by_version_file(config):
     for docker_name, docker_version in config["docker_image_versions"].items():
         if docker_name in config["dockers"]["container"] and docker_version != "" and docker_version != None:
             old_tag = config["dockers"]["container"][docker_name]["fullname"]
-            prefix = old_tag.split(":")[0]
+            list = old_tag.split(":")[0]
+            prefix = ":".join(list[0:(len(list)-1)])
             config["dockers"]["container"][docker_name]["fullname"] = prefix + ":" + str(docker_version)
     #pprint.pprint(config["dockers"])
     return

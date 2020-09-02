fefd30bcccc0506994de38cd96b02e9b9d66df8a
dragon0486 <869071540@qq.com>
2020-09-02 17:15:08 +0800

    add kfserving

--- src/ClusterBootstrap/services/istio/pre-render.sh
+++ src/ClusterBootstrap/services/istio/pre-render.sh
@@ -1,4 +1,4 @@
-if [ "$1" == "istio"];
+if [ "$1" == "istio" ];
 then
   curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.8 sh -
   istio-1.6.8/bin/istioctl install -f services/istio/istio.yaml --set values.global.jwtPolicy=first-party-jwt --force

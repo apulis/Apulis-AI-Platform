b31981c0c556c97370bd9e7931599cdda52975e8
dragon0486 <869071540@qq.com>
2020-09-21 20:37:29 +0800

    fixbug

--- src/ClusterManager/node_manager.py
+++ src/ClusterManager/node_manager.py
@@ -158,6 +158,7 @@ def get_cluster_status():
                         if "type" in condi and condi["type"] == "Ready" and "status" in condi and condi["status"] != "True":
                             node_status["unschedulable"] = True
 
+
                 nodes_status[node_status["name"]] = node_status
 
         output = k8sUtils.kubectl_exec(" get pods -o yaml")
@@ -259,7 +260,7 @@ def get_cluster_status():
                 if node_status["gpuType"] and node_status["gpuType"] in gpuMapping:
                     gpuMapping[node_status["gpuType"]]["detail"].append({"nodeName": node_name,
                                                                      "capacity": int(node_status["gpu_capacity"][node_status["gpuType"]]) if node_status["gpuType"] in node_status["gpu_capacity"] else 0,
-                                                                     "allocatable": max(int(node_status["gpu_allocatable"][node_status["gpuType"]])-int(node_status["gpu_used"][node_status["gpuType"]]),0)
+                                                                     "allocatable": max(int(node_status["gpu_allocatable"][node_status["gpuType"]])-int(node_status["gpu_used"][node_status["gpuType"]]) if node_status["gpuType"] in node_status["gpu_used"] else 0,0)
                                                                      if node_status["gpuType"] in node_status["gpu_allocatable"] else 0})
             gpu_used.Add(ResourceInfo(node_status["gpu_used"]))
             gpu_capacity.Add(ResourceInfo(node_status["gpu_capacity"]))

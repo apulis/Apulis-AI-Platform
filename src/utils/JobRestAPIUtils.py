4f21f1155b2957ab342b75ee69bdd17ae43cb953
dragon0486 <869071540@qq.com>
2020-12-14 12:07:34 +0800

    fixbug for kfserving

--- src/utils/JobRestAPIUtils.py
+++ src/utils/JobRestAPIUtils.py
@@ -1941,16 +1941,17 @@ def GetInferenceModel(model_id):
     models = []
     if "items" in deploys:
         for deploy in deploys["items"]:
-            models.append({
-                "description":deploy["metadata"]['annotations'].get('description',""),
-                "id":deploy["metadata"]['annotations'].get('id',""),
-                "kind":deploy["metadata"]['annotations'].get('type'),
-                "labels": [item['name'] for item in json.loads(deploy['metadata']['annotations'].get('spec') or '[]')],
-                "min_pos_points":int(deploy['metadata']['annotations'].get('min_pos_points', 1)),
-                "name":deploy['metadata']['annotations'].get('name', deploy["metadata"]["name"]),
-                "state":"ready" if deploy['status'].get('readyReplicas',0)>=1 else "error",
-                "framework":deploy['metadata']['annotations'].get('framework'),
-            })
+            if deploy["status"].get("availableReplicas",0)>0:
+                models.append({
+                    "description":deploy["metadata"]['annotations'].get('description',""),
+                    "id":deploy["metadata"]['annotations'].get('id',""),
+                    "kind":deploy["metadata"]['annotations'].get('type'),
+                    "labels": [item['name'] for item in json.loads(deploy['metadata']['annotations'].get('spec') or '[]')],
+                    "min_pos_points":int(deploy['metadata']['annotations'].get('min_pos_points', 1)),
+                    "name":deploy['metadata']['annotations'].get('name', deploy["metadata"]["name"]),
+                    "state":"ready" if deploy['status'].get('readyReplicas',0)>=1 else "error",
+                    "framework":deploy['metadata']['annotations'].get('framework'),
+                })
     if model_id:
         models = [i for i in models if i["id"]==model_id]
         if models:

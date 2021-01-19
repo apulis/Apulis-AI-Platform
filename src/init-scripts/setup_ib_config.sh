df3432e6ae36f53ba10b3c8e4a30b7cadc9feb46
dragon0486 <869071540@qq.com>
2021-01-19 16:39:41 +0800

    modify jobmanager

--- src/init-scripts/setup_ib_config.sh
+++ src/init-scripts/setup_ib_config.sh
@@ -56,7 +56,9 @@ then
     port=$(printenv $port_key)
 
     ENV_FILE=/pod.env
-    printf "export DLWS_SD_${DLWS_ROLE_NAME}${DLWS_ROLE_IDX}_IB_IP=${interface_ip}" >> "${ENV_FILE}" ;
+    SSH_ENVIRONMENT_FILE=/home/${DLWS_USER_NAME}/.ssh/environment
+    printf "export DLWS_SD_${DLWS_ROLE_NAME}${DLWS_ROLE_IDX}_IB_IP=${interface_ip}" >> "${ENV_FILE}";
+    printf "export DLWS_SD_${DLWS_ROLE_NAME}${DLWS_ROLE_IDX}_IB_IP=${interface_ip}" >> "${SSH_ENVIRONMENT_FILE}";
 
 WORKER_IB_CONFIG_FILE=/job/.ib_config
 if [ ! -f $WORKER_IB_CONFIG_FILE ];then touch $WORKER_IB_CONFIG_FILE;fi

9860af4724a96f1ea18ea3ce81233004b58d591c
dragon0486 <869071540@qq.com>
2021-01-19 18:39:54 +0800

    modify jobmanager

--- src/init-scripts/init_user.sh
+++ src/init-scripts/init_user.sh
@@ -53,11 +53,13 @@ grep -qx "^\s*. ${ENV_FILE}" /home/${DLWS_USER_NAME}/.profile || cat << SCRIPT >
 if [ -f ${ENV_FILE} ]; then
     . ${ENV_FILE}
 fi
+
+SCRIPT
+grep -qx "^\s*. ${ENVIRONMENT_FILE}" /home/${DLWS_USER_NAME}/.profile || cat << SCRIPT >> "/home/${DLWS_USER_NAME}/.profile"
 if [ -f ${ENVIRONMENT_FILE} ]; then
     . ${ENVIRONMENT_FILE}
 fi
 SCRIPT
-
 
 
 # any command should run as ${DLWS_USER_NAME}
